40d53d7a1223238637ea62ed088317fb2b1246eb
jason.lu <jason.lu@staff.nueip.com>
2019-10-03 09:47:59 +0800

    test

--- application/controllers/Hello_bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATTH') OR exit('No direct script access allowed');
-
-class Hello_bootstrap extends CI_Controller{
-    public function index(){
-        $this->load->view("helloBootstrap");
-    }
-    
-}
--- application/views/news/index.php
+++
@@ -1,4 +0,0 @@
-<?php foreach ($news as $news_item): ?>
-<h3><?php echo $news_item["title"] ?></h3>
-<p><?php echo $news_item["text"] ?><p>
-<?php endforeach?>
